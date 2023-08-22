94d3ace04eab1d806c7d663c5b779a0cacf5c1e5
nikhil.thakur <nikhil.thakur@altruistindia.com>
2023-08-22 17:58:02 +0530

    another one added

--- src/components/Services/Sidebar.js
+++ src/components/Services/Sidebar.js
@@ -2,6 +2,7 @@
 import { axiosApi } from "@/axios";
 import React, { useLayoutEffect, useRef, useState } from "react";
 import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Sidebar = () => {
   const [loading, setloading] = useState(true);
@@ -34,55 +35,65 @@ const Sidebar = () => {
 
   useLayoutEffect(() => {
     getAllServices();
-    const button = ref.current;
 
-    if (button) {
-      button.click();
+    if (typeof window !== undefined) {
+      if (typeof document !== undefined) {
+        if (first?.data?.length > 0) {
+          console.log("here i am");
+          setTimeout(() => {
+            document.getElementById("sidebar0").click();
+          }, 500);
+        }
+      }
     }
   }, []);
+
+  console.log(first.data, "arrau");
 
   return (
     <section className="main_section mt-5 mb-5">
       <div className="container">
         <div className="row">
           <div className="col-md-3">
-            <aside className="left_sidebar">
-              {loading ? (
-                <>
-                  <Skeleton count={18} />
-                </>
-              ) : (
-                <ul className="sidebar_menu">
-                  {first?.data?.map((evale, index) => {
-                    return (
-                      <li key={index}>
-                        <a
-                          // key={index}
-                          className={
-                            first?.name === evale?.name
-                              ? "sidebar-active"
-                              : "sidebar"
-                          }
-                          onClick={() => {
-                            setfirst({
-                              ...first,
-                              mainText: evale?.mainText,
-                              leftImage: evale?.leftImage,
-                              rightText: evale?.rightText,
-                            });
-                          }}
-                          id={`sidebar${index}`}
-                          ref={ref}
-                          style={{ cursor: "pointer" }}
-                        >
-                          {evale?.name}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
-              )}
-            </aside>
+            {loading ? (
+              <>
+                <Skeleton count={13} />
+              </>
+            ) : (
+              <>
+                <aside className="left_sidebar">
+                  <ul className="sidebar_menu">
+                    {first?.data?.map((evale, index) => {
+                      return (
+                        <li key={index}>
+                          <a
+                            // key={index}
+                            className={
+                              first?.name === evale?.name
+                                ? "sidebar-active"
+                                : "sidebar"
+                            }
+                            onClick={() => {
+                              setfirst({
+                                ...first,
+                                mainText: evale?.mainText,
+                                leftImage: evale?.leftImage,
+                                rightText: evale?.rightText,
+                              });
+                            }}
+                            id={`sidebar${index}`}
+                            ref={ref}
+                            style={{ cursor: "pointer" }}
+                          >
+                            {evale?.name}
+                          </a>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </aside>
+              </>
+            )}
           </div>
           <div className="col-md-9">
             <div className="right_content solution_content">
@@ -91,7 +102,7 @@ const Sidebar = () => {
                   <Skeleton
                     count={1}
                     enableAnimation={true}
-                    style={{ width: "100%", height: "425px" }}
+                    style={{ width: "100%", height: "300px" }}
                   />
                 </>
               ) : (
